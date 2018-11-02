0fa9bdf05940fca5f20b05c8a0f1fa69f715ee33
Kevin Chin <chinkevin25@gmail.com>
2018-11-02 16:26:05 -0700

    finished demo

---
+++ src/containers/book-detail.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class BookDetail extends Component {
+  render() {
+
+    if(!this.props.book) {
+      return <div>Select a book to get started</div>
+    }
+
+    return (
+      <div> 
+        <h3> Details for: </h3>
+        <div> Title: {this.props.book.title} </div> 
+        <div> Pages: {this.props.book.pages} </div> 
+      </div> 
+    );
+  }
+}
+
+function mapStatesToProps(state) {
+  return {
+    book :state.activeBook
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  // Don't need this one because there are no actions.
+}
+
+export default connect(mapStatesToProps)(BookDetail);
